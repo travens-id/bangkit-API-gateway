a354d7400b8aed6080774c042eb9eea35f9f65fc
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2022-05-14 12:45:15 +0700

    feat: user routes and auth routes

--- routes/authRouter.go
+++ routes/authRouter.go
@@ -1,1 +1,12 @@
 package routes
+
+import (
+	controller "travens/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.POST("users/signup", controller.Signup())
+	incomingRoutes.POST("users/login", controller.Login())
+}
--- routes/userRouter.go
+++ routes/userRouter.go
@@ -1,1 +1,14 @@
 package routes
+
+import (
+	controller "travens/controllers"
+	middleware "travens/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.Use(middleware.Authenticate())
+	incomingRoutes.GET("/users", controller.GetUsers())
+	incomingRoutes.GET("/users/:user_id", controller.GetUsers())
+}
