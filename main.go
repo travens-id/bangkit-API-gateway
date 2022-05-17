cbca69e5c29c8b4d61a041a28e00f22d12419e1b
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2022-05-17 15:35:09 +0700

    finish but error dependencies

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"log"
 	"os"
 	routes "travens/routes"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	port := os.Getenv("PORT")
 
 	if port == "" {
@@ -21,11 +28,11 @@ func main() {
 	routes.UserRoutes(router)
 
 	router.GET("/api-1", func(c *gin.Context) {
-		c.JSON(200, gin.H{"success": "access granted for api-1"})
+		c.JSON(200, gin.H{"success": "Access granted for api-1"})
 	})
 
 	router.GET("/api-2", func(c *gin.Context) {
-		c.JSON(200, gin.H{"success": "access granted for api-2"})
+		c.JSON(200, gin.H{"success": "Access granted for api-2"})
 	})
 
 	router.Run(":" + port)
